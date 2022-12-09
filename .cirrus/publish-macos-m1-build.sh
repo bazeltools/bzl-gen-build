58c63e947339d4b776041fffa62e33a0c0e52cab
Ian O Connell <ianoc@ianoc.net>
2022-12-08 17:03:54 -0700

    Fix path

--- .cirrus/publish-macos-m1-build.sh
+++ .cirrus/publish-macos-m1-build.sh
@@ -8,6 +8,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 export PATH=$PATH:$HOME/.cargo/bin
 
+WORKING_DIRECTORY=`pwd`
+
 ./.github/ci_scripts/prepare_output.sh bzl-gen-build-macos-arm64.tgz staging-directory
 
 
@@ -34,7 +36,7 @@ do
   name=$(basename "$fpath")
   url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
   curl -X POST \
-    --data-binary @$fpath \
+    --data-binary @$WORKING_DIRECTORY/stagin-directory/$fpath \
     --header "Authorization: token $GITHUB_TOKEN" \
     --header "Content-Type: $file_content_type" \
     $url_to_upload
