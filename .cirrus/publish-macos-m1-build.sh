581dda7ae67cee0b9da6cff7b77371e6c6917cd7
Ian O'Connell <ianoc@ianoc.net>
2022-12-09 06:50:59 -0700

    Update publish-macos-m1-build.sh

--- .cirrus/publish-macos-m1-build.sh
+++ .cirrus/publish-macos-m1-build.sh
@@ -25,16 +25,16 @@ fi
 file_content_type="application/octet-stream"
 
 files_to_upload=(
-  $WORKING_DIRECTORY/staging-directory/bzl-gen-build-macos-arm64.tgz
   $WORKING_DIRECTORY/staging-directory/bzl-gen-build-macos-arm64.tgz.sha256
+  $WORKING_DIRECTORY/staging-directory/bzl-gen-build-macos-arm64.tgz
 )
-
+ls $WORKING_DIRECTORY/staging-directory
 for fpath in $files_to_upload
 do
   echo "Uploading $fpath..."
   name=$(basename "$fpath")
   url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
-  curl --fail -X POST \
+  curl -v --fail -X POST \
     --data-binary @$fpath \
     --header "Authorization: token $GITHUB_TOKEN" \
     --header "Content-Type: $file_content_type" \
