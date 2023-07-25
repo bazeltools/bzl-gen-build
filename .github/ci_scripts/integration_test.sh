cdec70c70f4aab9610c547ffd076de0a15a3d92d
Eugene Yokota <eed3si9n@gmail.com>
2023-07-25 16:22:01 -0400

    Check that git diff is clean after regenerate

--- .github/ci_scripts/integration_test.sh
+++ .github/ci_scripts/integration_test.sh
@@ -8,3 +8,10 @@ cd example
 # TOOLING_WORKING_DIRECTORY=/tmp/bzl-gen-build source build_tools/lang_support/create_lang_build_files/regenerate_protos_build_files.sh
 TOOLING_WORKING_DIRECTORY=/tmp/bzl-gen-build source build_tools/lang_support/create_lang_build_files/regenerate_python_build_files.sh
 bazel test ...
+
+changes=$(git diff --name-only --diff-filter=ACMRT | xargs)
+if [ ! -z "$changes" ]; then
+    echo "::error file=example/WORKSPACE::Generated $changes differs from the checked-in version"
+    git diff --exit-code
+    exit 1
+fi
