3ec38bd396a3cf2473cbf227b6ff5631603a9f7c
eugene yokota <eed3si9n@gmail.com>
2024-02-29 20:33:03 -0500

    Improve Python indexing example (#227)
    **Problem**
    In the current example Python indexing only tracks
    3rdparty libraries, but not Protobuf-generated sources.
    
    **Solution**
    This adjusts the scripts to index both 3rdparty pip targets
    and Protobuf-generated sources.

---
+++ example/build_tools/lang_support/create_lang_build_files/regenerate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+if [ -n "${INVOKED_VIA_BAZEL:-}" ]; then
+    REPO_ROOT="$BUILD_WORKING_DIRECTORY"
+else
+    REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../../ && pwd )"
+fi
+
+source "$REPO_ROOT/build_tools/lang_support/create_lang_build_files/delete_build_files.sh"
+source "$REPO_ROOT/build_tools/lang_support/create_lang_build_files/regenerate_protos_build_files.sh"
+source "$REPO_ROOT/build_tools/lang_support/create_lang_build_files/regenerate_python_build_files.sh"
+source "$REPO_ROOT/build_tools/lang_support/create_lang_build_files/regenerate_jvm_build_files.sh"
