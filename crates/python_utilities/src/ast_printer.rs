66aa4eb8845e9426a13f39d59082e83e732c607c
Eugene Yokota <eed3si9n@gmail.com>
2024-08-23 13:06:41 -0400

    Pretty-print BUILD file a la buildifier
    **Problem**
    Currently we have to run buildifier to pretty print the BUILD file,
    which takes human-observable time.
    
    **Solution**
    This pretty-prints it in buildifier style so we don't have to wait.

--- crates/python_utilities/src/ast_printer.rs
+++ crates/python_utilities/src/ast_printer.rs
@@ -15,6 +15,7 @@ struct WritingBuffer<'a> {
     buf: Vec<Cow<'a, str>>,
     indent: usize,
     in_line: bool,
+    in_keyword: bool,
 }
 
 impl<'a> WritingBuffer<'a> {
@@ -23,6 +24,7 @@ impl<'a> WritingBuffer<'a> {
             buf: Vec::default(),
             indent: 0,
             in_line: true,
+            in_keyword: false,
         }
     }
     pub fn indent(&mut self) -> &mut Self {
@@ -31,7 +33,7 @@ impl<'a> WritingBuffer<'a> {
     }
 
     pub fn deindent(&mut self) -> &mut Self {
-        self.indent += 1;
+        self.indent -= 1;
         self
     }
 
@@ -39,7 +41,7 @@ impl<'a> WritingBuffer<'a> {
         if !self.in_line {
             self.buf.push(Cow::Borrowed("\n"));
             for _ in 0..self.indent {
-                self.buf.push(Cow::Borrowed("  "))
+                self.buf.push(Cow::Borrowed("    "))
             }
         }
         self.in_line = true;
@@ -52,6 +54,14 @@ impl<'a> WritingBuffer<'a> {
 
     pub fn finish(self) -> String {
         self.buf.join("")
+    }
+
+    pub fn begin_keyword(&mut self) {
+        self.in_keyword = true
+    }
+
+    pub fn finish_keyword(&mut self) {
+        self.in_keyword = false
     }
 }
 
@@ -154,12 +164,155 @@ fn emit_body<'a>(body: &'a [Stmt], str_buffer: &mut WritingBuffer<'a>) {
             }
 
             Stmt::Expr(ast::StmtExpr { range: _, value }) => {
-                str_buffer.push(Cow::Owned(format!("{}", value)));
+                let expr: &ast::Expr = &*value;
+                CustomDisplay::custom_fmt(expr, str_buffer, false);
                 str_buffer.finish_line()
             }
             _ => {
                 panic!("Have not implemented how to print: {:?}", stmt)
             }
+        }
+    }
+}
+
+/**
+ * CustomDisplay represents a mutable pretty printing.
+ * WritingBuffer keep track of the indentation state, so we can't just return string.
+ * This allows, e.g. a nested list items to be double-indented.
+ *
+ * defer - When true, it does not push to str_buffer, and just returns the String value.
+ *         This functionality is partially implemented since we need it only to pick up
+ *         the function name.
+ */
+trait CustomDisplay {
+    fn custom_fmt(&self, str_buffer: &mut WritingBuffer, defer: bool) -> String;
+}
+
+impl CustomDisplay for ast::Expr {
+    fn custom_fmt(&self, str_buffer: &mut WritingBuffer, defer: bool) -> String {
+        fn push(str_buffer: &mut WritingBuffer, defer: bool, s: String) -> String {
+            if !defer {
+                str_buffer.push(Cow::Owned(s.clone()));
+            }
+            s
+        }
+        fn push_inline_list(
+            str_buffer: &mut WritingBuffer,
+            defer: bool,
+            list: &Vec<ast::Expr>,
+        ) -> String {
+            str_buffer.push(Cow::Owned("[".to_string()));
+            let mut idx: usize = 0;
+            for elem in list.iter() {
+                elem.custom_fmt(str_buffer, defer);
+                if idx < list.len() - 1 {
+                    str_buffer.push(Cow::Owned(", ".to_string()))
+                }
+                idx += 1;
+            }
+            str_buffer.push(Cow::Owned("]".to_string()));
+            "".to_string()
+        }
+        fn push_multi_line_list(
+            str_buffer: &mut WritingBuffer,
+            defer: bool,
+            list: &Vec<ast::Expr>,
+        ) -> String {
+            str_buffer.push(Cow::Owned("[".to_string()));
+            str_buffer.finish_line();
+            str_buffer.indent();
+            for elem in list.iter() {
+                elem.custom_fmt(str_buffer, defer);
+                str_buffer.push(Cow::Owned(",".to_string()));
+                str_buffer.finish_line();
+            }
+            str_buffer.deindent();
+            str_buffer.push(Cow::Owned("]".to_string()));
+            "".to_string()
+        }
+
+        match self {
+            // This uses double-quotation for String literals
+            ast::Expr::Constant(ast::ExprConstant { value, .. }) => match value {
+                ast::Constant::Str(str) => push(str_buffer, defer, format!("\"{}\"", str)),
+                _ => push(str_buffer, defer, format!("{}", self)),
+            },
+            ast::Expr::Call(ast::ExprCall {
+                func,
+                args,
+                keywords,
+                ..
+            }) => {
+                let func_expr: &ast::Expr = &*func;
+                let name = func_expr.custom_fmt(str_buffer, true);
+                if name == "load" {
+                    str_buffer.push(Cow::Owned(name));
+                    str_buffer.push(Cow::Owned("(".to_string()));
+                    let mut idx: usize = 0;
+                    for arg in args.iter() {
+                        arg.custom_fmt(str_buffer, defer);
+                        if idx < args.len() - 1 {
+                            str_buffer.push(Cow::Owned(", ".to_string()))
+                        }
+                        idx += 1;
+                    }
+                    idx = 0;
+                    for kw in keywords.iter() {
+                        str_buffer.begin_keyword();
+                        match &kw.arg {
+                            Some(arg) => {
+                                str_buffer.push(Cow::Owned(format!("{} = ", arg)));
+                            }
+                            None => (),
+                        };
+                        kw.value.custom_fmt(str_buffer, defer);
+                        if idx < keywords.len() - 1 {
+                            str_buffer.push(Cow::Owned(", ".to_string()))
+                        }
+                        str_buffer.finish_keyword();
+                        idx += 1;
+                    }
+                    str_buffer.push(Cow::Owned(")".to_string()));
+                } else {
+                    if !str_buffer.in_keyword {
+                        str_buffer.push(Cow::Owned("".to_string()));
+                        str_buffer.finish_line();
+                    } 
+                    str_buffer.push(Cow::Owned(name));
+                    str_buffer.push(Cow::Owned("(".to_string()));
+                    str_buffer.finish_line();
+                    str_buffer.indent();
+                    for arg in args.iter() {
+                        arg.custom_fmt(str_buffer, defer);
+                        str_buffer.push(Cow::Owned(",".to_string()));
+                        str_buffer.finish_line();
+                    }
+                    for kw in keywords.iter() {
+                        str_buffer.begin_keyword();
+                        match &kw.arg {
+                            Some(arg) => {
+                                str_buffer.push(Cow::Owned(format!("{} = ", arg)));
+                            }
+                            None => (),
+                        };
+                        kw.value.custom_fmt(str_buffer, defer);
+                        str_buffer.push(Cow::Owned(",".to_string()));
+                        str_buffer.finish_keyword();
+                        str_buffer.finish_line();
+                    }
+                    str_buffer.deindent();
+                    str_buffer.push(Cow::Owned(")".to_string()));
+                }
+                "".to_string()
+            }
+            ast::Expr::List(ast::ExprList { elts, .. }) => {
+                if elts.len() < 2 {
+                    push_inline_list(str_buffer, defer, elts)
+                } else {
+                    push_multi_line_list(str_buffer, defer, elts)
+                }
+            }
+            _ => push(str_buffer, defer, format!("{}", self)),
         }
     }
 }
@@ -175,16 +328,57 @@ mod tests {
     use super::*;
 
     #[test]
-    fn round_trip_parsing() {
-        let python_source = r#"from tensorflow import foo
-def cust_fn():
-  pass"#;
+    fn round_trip_build_file() {
+        assert_round_trip(
+            r#"load("@rules_proto//proto:defs.bzl", "proto_library")
 
-        let parsed = PythonProgram::parse(python_source, "tmp.py").unwrap();
+proto_library(
+    name = "aa_proto",
+    srcs = ["aa.proto"],
+    deps = [
+        "//x",
+        "//y",
+    ],
+    nested = [
+        ["foo"],
+        ["bar"],
+    ],
+    visibility = ["//visibility:public"],
+)
+
+java_proto_library(
+    name = "a_proto_java",
+    visibility = ["//visibility:public"],
+    deps = [":a_proto"],
+)
+
+filegroup(
+    name = "example_files",
+    srcs = glob(
+        include = ["**/*.java"],
+    ),
+    visibility = ["//visibility:public"],
+)"#,
+        )
+    }
+
+    #[test]
+    fn round_trip_python_source() {
+        assert_round_trip(
+            r#"from tensorflow import foo
+def aa():
+    pass
+def cust_fn():
+    pass"#,
+        )
+    }
+
+    fn assert_round_trip(code: &str) {
+        let parsed = PythonProgram::parse(code, "tmp.py").unwrap();
         let printed_parsed = format!("{}", parsed);
 
         assert_eq!(
-            python_source, printed_parsed,
+            code, printed_parsed,
             "\n\nPrinted parsed was: {}\n\n",
             printed_parsed
         );
