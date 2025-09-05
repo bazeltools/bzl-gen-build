eb74ea2c4782f9776ce3fa8421bf338537eaf307
Klukas <jklukas@netflix.com>
2025-09-05 13:45:49 -0400

    Add blank lines after generated code comments

--- crates/driver/src/print_build.rs
+++ crates/driver/src/print_build.rs
@@ -404,8 +404,10 @@ impl TargetEntries {
         let program = self.to_ast()?;
         Ok(format!(
             "# ---- BEGIN BZL_GEN_BUILD_GENERATED_CODE ---- no_hash
+
 {}
 # ---- END BZL_GEN_BUILD_GENERATED_CODE ---- no_hash
+
 ",
             &program
         ))
