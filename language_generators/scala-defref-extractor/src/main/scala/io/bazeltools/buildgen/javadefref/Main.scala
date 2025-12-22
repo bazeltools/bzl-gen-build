7979b00d2ef17a5b51d0db35fe9b3ba58a1d1407
bayarmunkh <bdavaadorj@netfli.com>
2025-12-22 17:35:21 -0500

    explain why specialTlds unused

--- language_generators/scala-defref-extractor/src/main/scala/io/bazeltools/buildgen/javadefref/Main.scala
+++ language_generators/scala-defref-extractor/src/main/scala/io/bazeltools/buildgen/javadefref/Main.scala
@@ -5,6 +5,7 @@ import io.bazeltools.buildgen.shared.{Symbols, DriverApplication}
 
 object Main extends DriverApplication {
   def name: String = "java_extractor"
+  // The Java extractor doesn't need special TLD logic for wildcard import optimization
   def extract(data: String, _specialTlds: Set[String]): IO[Symbols] =
     JavaSourceEntityExtractor.extract(data)
 }
